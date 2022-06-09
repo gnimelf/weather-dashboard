bce851dc2a366900bf8026078590f842278e915d
Justin <fezzygo@gmail.com>
2022-06-09 00:02:15 -0400

    removed unnecessary selectors (script.js:2-10), added function for performing weather search and getting 5 day forecast (script.js:12-43)

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,13 +1,5 @@
-var searchInputEl=$("#search-input");
-var searchButtonEl=$("#search-button");
-var columbusEl=$("#columbus-btn");
-var chicagoEl=$("#chicago-btn");
-var newYorkEl=$("#new-york-btn");
-var orlandoEl=$("#orlando-btn");
-var sanFranciscoEl=$("#san-francisco-btn");
-var seattleEl=$("#seattle-btn");
-var denverEl=$("#denver-btn");
-var atlantaEl=$("#atlanta-btn");
+var buttonEl=$("button");
+var cityInput=$("#search-input");
 var cityDateIconEl=$("#city-date-icon");
 var currentTempEl=$("#current-temp");
 var currentWindEl=$("#current-wind");
@@ -17,9 +9,29 @@ var currentUVIndexEl=$("#current-uv-index");
 var openWeatherAPI = "https://api.openweathermap.org/data/2.5/forecast?q="
 var city = "";
 var key = "6c03b15832f909d67599d2b7a3dc73ff";
+var weatherDate = {};
 
+function performSearch(event){
+    var cityToSearch = "";
+    if (event.target.id === "search-button"){
+        cityToSearch = cityInput.val();
+        // console.log(cityToSearch);
 
-// Weather card
+    } else {
+        // console.log(event.target.textContent);
+        cityToSearch = event.target.textContent;
+    }
+
+    // get current weather
+
+    // get five day forecast
+    fiveDayForecast();
+}
+
+buttonEl.on("click", performSearch);
+
+function fiveDayForecast(){
+    // Weather card
     // <section class="card">
     // <h3>date</h3>
     // <i>icon</i>
@@ -27,3 +39,4 @@ var key = "6c03b15832f909d67599d2b7a3dc73ff";
     // <p>Wind:</p>
     // <p>Humity:</p>
     //     </section>
+}
