c10dc5189a31636289434da924f6ae7991f81300
Justin <fezzygo@gmail.com>
2022-06-09 23:38:14 -0400

    started currentWeather func (js:46-51), almost finished fiveDayForecast func (js:56-96), added createCard func (js:98-123)

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -5,13 +5,14 @@ var currentTempEl=$("#current-temp");
 var currentWindEl=$("#current-wind");
 var currentHumityEl=$("#current-humity");
 var currentUVIndexEl=$("#current-uv-index");
-var time 
+var forecastCardsEl = $("#forecast-cards");
+var todaysDate = moment().format("YYYY-MM-DD");
 
 var openWeatherAPI = "https://api.openweathermap.org/data/2.5/forecast?"
 var city = "";
 var units = "imperial"
 var key = "6c03b15832f909d67599d2b7a3dc73ff";
-var weatherDate = {};
+var weatherData = "";
 
 function performSearch(event){
     var cityToSearch = "";
@@ -33,33 +34,92 @@ function performSearch(event){
             console.log("connection failed " + response.status)
         }
     }).then(data => {
-        console.log(data);
+        fiveDayForecast(data);
     });
 
     // TODO: grab current weather
-    
+
     // TODO: grab five day forecast
-    fiveDayForecast();
+    
 }
 
-function currentWeather(data){
+function currentWeather(){
     
-    var currentItem = data.list[0].weather[0]
-    var date = moment.unix(currentItem.dt).format("M/D/YYYY")
-    var currentIcon = `http://openweathermap.org/img/wn/${currentItem}@2x.png`
+    var city = date.city.name;
+    var currentItem = data.list[0].weather[0];
+    var date = moment.unix(currentItem.dt).format("M/D/YYYY");
+    // var currentIcon = `http://openweathermap.org/img/wn/${currentItem}@2x.png`;
     var currentTemp = data.list[0].weather[0]
     
 }
 
-function fiveDayForecast(data){
-    // Weather card
-    // <section class="card">
-    // <h3>date</h3>
-    // <i>icon</i>
-    // <p>Temp:</p>
-    // <p>Wind:</p>
-    // <p>Humity:</p>
-    // </section>
+function fiveDayForecast(weatherData){
+    console.log(weatherData);
+    var forecastData = weatherData.list;
+    var todaysDate = moment().format("YYYY-MM-DD");
+    var nextDayWeather = moment().add(1,"day").format("YYYY-MM-DD");
+    var thirdDayWeather = moment().add(2,"day").format("YYYY-MM-DD");
+    var fourthDayWeather = moment().add(3,"day").format("YYYY-MM-DD");
+    var fiveDayWeather = moment().add(4,"day").format("YYYY-MM-DD");
+    var foundToday = false;
+    var foundNextDay = false;
+    var foundThirdDay = false;
+    var foundFourthDay = false;
+    var foundFifthDay = false;
+
+    for (var i=0; i<forecastData.length; i++){
+        nextDay = moment().add(i,"day").format("YYYY-MM-DD");
+        var item = forecastData[i];
+        var itemDate = item.dt_txt.split(" ")[0];
+        // Get the current day weather a item 0
+        if (itemDate === todaysDate && !foundToday) {
+            console.log("found today");
+            
+        } else if(itemDate === nextDayWeather && !foundNextDay) {
+            console.log("found nextDay");
+            foundNextDay = true;
+            createCard(item, itemDate);
+        } else if(itemDate === thirdDayWeather && !foundThirdDay) {
+            console.log("found thirDay");
+            foundThirdDay = true;
+            createCard(item, itemDate);
+        } else if(itemDate === fourthDayWeather && !foundFourthDay) {
+            console.log("found fourthDay");
+            foundFourthDay = true;
+            createCard(item, itemDate);
+        } else if(itemDate === fiveDayWeather && !foundFifthDay) {
+            console.log("found fifthDay");
+            foundFifthDay = true;
+            createCard(item, itemDate);
+        }
+    }
+}
+
+function createCard(item, itemDate){
+    var currentIcon = `http://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`;
+    var cardEl = $("<section>");
+    cardEl.attr("class","card");
+
+    var dateEl = $("<h3>");
+    dateEl.text(itemDate);
+    cardEl.append(dateEl);
+    
+    var imgEl = $("<img>");
+    imgEl.attr("src", currentIcon);
+    cardEl.append(imgEl);
+    
+    var tempEl = $("<p>");
+    tempEl.text(`Temp: ${item.main.temp}`);
+    cardEl.append(tempEl);
+    
+    var windEl = $("<p>");
+    windEl.text(`Temp: ${item.wind.speed}`);
+    cardEl.append(windEl);
+    
+    var humidityEl = $("<p>");
+    humidityEl.text(`Humity: ${item.main.humidity}`);
+    cardEl.append(humidityEl);
+    forecastCardsEl.append(cardEl);
 }
 
 buttonEl.on("click", performSearch);
